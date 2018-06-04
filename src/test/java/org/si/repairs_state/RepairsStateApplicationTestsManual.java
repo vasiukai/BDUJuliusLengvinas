e393a92ded623f3baf8c1d9dc502ee1c6bc17d35
༼ つ ◕_◕ ༽つ <vasiukai@users.noreply.github.com>
2018-06-04 22:45:26 +0300

    Update and rename RepairsStateApplicationTests.java to RepairsStateApplicationTestsManual.java

--- src/test/java/org/si/repairs_state/RepairsStateApplicationTestsManual.java
+++ src/test/java/org/si/repairs_state/RepairsStateApplicationTestsManual.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RepairsStateApplicationTests {
+public class RepairsStateApplicationTestsManual {
 
 	@Test
 	public void contextLoads() {
